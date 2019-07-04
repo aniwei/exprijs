f4ec59cbfcc6a491bdfb67a827ce27ff75b62937
aniwei <aniwei@shanlaan.com>
2019-07-04 18:08:33 +0800

    update

---
+++ src/scheduler/worker/commitAllWork.js
@@ -0,0 +1,16 @@
+import commitWork from './commitWork';
+import worker from './index';
+
+
+export default function commitAllWork (fiber) {
+  fiber.effects.forEach(f => {
+    commitWork(f);
+  });
+
+  
+  fiber.stateNode._rootContainerFiber = fiber;
+  
+  worker.nextUnitOfWork = null;
+  worker.pendingCommit = null;
+}
+
