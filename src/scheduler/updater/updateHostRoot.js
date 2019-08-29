71619feef414021b3553e105d3aca1a88627d5da
Administrator <admin@example.com>
2019-08-30 01:54:48 +0800

    update

--- src/scheduler/updater/updateHostRoot.js
+++ src/scheduler/updater/updateHostRoot.js
@@ -1,5 +1,6 @@
 import pushHostRootContext from '../../context/pushHostRootContext';
 import reconcileChildren from '../../reconciler/reconcileChildren';
+import { PLACEMENT } from '../../shared/effectTags';
 
 export default function updateHostRoot (
   current,
@@ -27,14 +28,13 @@ export default function updateHostRoot (
     if (children === prevChildren) {
       return bailoutOnAlreadyFinishedWork(current, workInProgress);
     }
+
     const root = workInProgress.stateNode;
     
     if (
-      (current === null || current.child === null) &&
-      root.hydrate &&
-      enterHydrationState(workInProgress)
+      (isNull(current) || isNull(current.child))
     ) {
-      workInProgress.effectTag |= Placement;
+      workInProgress.effectTag |= PLACEMENT;
 
       workInProgress.child = mountChildFibers(
         workInProgress,
@@ -45,11 +45,26 @@ export default function updateHostRoot (
     } else {
       reconcileChildren(current, workInProgress, nextChildren);
     }
+
     return workInProgress.child;
   }
   
-
   return bailoutOnAlreadyFinishedWork(current, workInProgress)
+}
+
+function bailoutOnAlreadyFinishedWork (
+  current,
+  workInProgress
+) {
+  if (!isNull(current)) {
+    workInProgress.firstContextDependency = current.firstContextDependency;
+  }
+
+  if (workInProgress.isNoWork) {
+    return null;
+  }
+
+  cloneChildFibers(current, workInProgress);
 
   return workInProgress.child;
 }
